6332b688ae95593dde58ea5a2c87ff53ad2e65e2
Luke <99337120+soyLuke03@users.noreply.github.com>
2022-03-01 08:13:43 +0100

    Juego de cartas
    Carpeta de trabajo del juego

---
+++ OrientacionAObjetos/Cartas 7-1&2/src/JuegoDeCartas/MainInterfaz.java
@@ -0,0 +1,16 @@
+package JuegoDeCartas;
+
+public class MainInterfaz {
+
+	public static void main(String[] args) {
+		
+		Cartas carta1 = new Cartas();
+		Cartas carta2 = new Cartas();
+		
+		
+		
+		
+		
+		
+	}
+}
